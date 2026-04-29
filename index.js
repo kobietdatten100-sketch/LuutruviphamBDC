972dd7948b5b39c26d265ae459a7fd5aee67d3f1
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-04-29 15:50:10 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+let data = [
+    {
+        name: "8rpgril",
+        rank: "Thiếu úy",
+        level: 5,
+        error: "Vi phạm nội quy",
+        penalty: "Giam quân hàm",
+        start: "2026-04-28",
+        end: "2026-5-28"
+    },
+    {
+        name: "utkhoQuan4",
+        rank: "Trung sĩ",
+        level: 5,
+        error: "Vi phạm nội quy",
+        penalty: "giam quân hàm",
+        start: "2026-04-28",
+        end: "2026-05-28"
+    }
+];
